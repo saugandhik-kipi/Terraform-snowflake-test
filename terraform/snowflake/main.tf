2c8c3abc06427dbd4088a36d55271031699b25a0
saugandhik-kipi <saugandhik.a.pal@kipi.bi>
2024-08-23 23:45:48 +0530

    Update main.tf

--- terraform/snowflake/main.tf
+++ terraform/snowflake/main.tf
@@ -1,19 +1,23 @@
 terraform {
- required_providers {
- snowflake = {
- source = "Snowflake-Labs/snowflake"
- version = "~> 0.68"
- }
- }
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.87"
+    }
+  }
 }
+
 provider "snowflake" {
- role = "SYSADMIN"
+  role = "SYSADMIN"
 }
+
 resource "snowflake_database" "db" {
- name = "TF_DEMO"
+  name = "TF_DEMO"
 }
+
 resource "snowflake_warehouse" "warehouse" {
- name = "TF_DEMO"
- warehouse_size = "large"
- auto_suspend = 60
+  name           = "TF_DEMO"
+  warehouse_size = "xsmall"
+  auto_suspend   = 120
+  
 }
